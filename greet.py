2e225242b7de15d57e6792d35bcf641c76bfa7e7
meak-c <chithonmeak@gmail.com>
2025-06-26 14:25:03 +0900

    2025-06-26-new

--- greet.py
+++ greet.py
@@ -1,12 +1,30 @@
 from sys import argv, exit
 
-if len(argv) != 3:
-    exit("名前と年齢の合計2つの引数を入力してください（例: python greet.py Alice 22）")
 
-name = argv[1]
-try:
-    age = int(argv[2])
-except ValueError:
-    exit("年齢は整数で入力してください")
+def parse_argv():
+    if len(argv) != 3:
+        exit("名前と年齢の合計2つの引数を入力してください（例: python greet.py Alice 22）")
 
-print(f"こんにちは、{name}さん（{age}歳）！")
+    name = argv[1]
+    try:
+        age = int(argv[2])
+    except ValueError:
+        exit("年齢は整数で入力してください")
+
+    if age < 0 or age > 130:
+        exit("正しい年齢を入力してください")
+
+    return name, age
+
+
+def greet(name, age):
+    print(f"こんにちは、{name}さん（{age}歳）！")
+
+
+def main():
+    name, age = parse_argv()
+    greet(name, age)
+
+
+if __name__ == "__main__":
+    main()
